60f2381ed41c6c1441ca7e14fc45f15a366856e1
LittleT <1211148525@qq.com>
2023-04-30 19:54:19 +0800

    add nacos-dev commit

--- Tmall-product/src/main/java/com/tym/Tmall/product/TmallProductApplication.java
+++ Tmall-product/src/main/java/com/tym/Tmall/product/TmallProductApplication.java
@@ -3,9 +3,11 @@ package com.tym.Tmall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.tym.Tmall.product.dao")
+@EnableDiscoveryClient
 public class TmallProductApplication {
 
     public static void main(String[] args) {
--- Tmall-ware/src/main/java/com/tym/Tmall/ware/TmallWareApplication.java
+++ Tmall-ware/src/main/java/com/tym/Tmall/ware/TmallWareApplication.java
@@ -2,8 +2,10 @@ package com.tym.Tmall.ware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TmallWareApplication {
     public static void main(String[] args) {
         SpringApplication.run(TmallWareApplication.class,args);
